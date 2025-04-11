07c3886f71997b4fe8ea4643ce24c124361c40af
sahnoune2 <anankarta@gmail.com>
2025-04-11 01:50:27 +0100

    done

--- tanitjobs-backend/server.js
+++ tanitjobs-backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const app = express();
 
